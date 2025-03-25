55e4579fd1745cb1edac85f0b3a1ff54600485c0
John-Hephaistos <vladmvasile@gmail.com>
2025-03-25 18:50:56 +0100

    sorting by stars count now the default sorting option - should work

--- backend/src/week/weekService.ts
+++ backend/src/week/weekService.ts
@@ -30,7 +30,7 @@ export class WeekService {
       const year = date.getFullYear();
       const month = (date.getMonth() + 1).toString().padStart(2, "0");
       const day = date.getDate().toString().padStart(2, "0");
-      const url = `http://127.0.0.1:8787/${year}-${month}-${day}`;
+      const url = `http://127.0.0.1:45943/${year}-${month}-${day}`;
       urls.push(url);
     }
     
